2667900c342b50590c24cc5cc97f6ae1dc82ad9e
bandojing <technofrosty@yahoo.com>
2020-02-05 20:37:35 +0000

    prints Programming is like building a multilingual puzzle, followed by a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <string.h>
+
 int main (void)
 {
  char string[47];
-  strcpy(str,""Programming is like building a multilingual puzzle\n");
+  strcpy(string,"\"Programming is like building a multilingual puzzle\n");
   puts(string);
 return 0;
 
