8b9b13d5d31d01c64c907fbaa286cebaea7ab481
bandojing <technofrosty@yahoo.com>
2020-02-06 20:06:58 +0000

    tells whether rand number is neg, pos, or zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,11 +17,11 @@ int main(void)
 	}
 	else if (n > 0)
 	{
-		printf("%d  is positive\n", n);
+		printf("%d is positive\n", n);
 	}
 		else if (n == 0)
 		{
-			printf("%d  is zero\n", n);
+			printf("%d is zero\n", n);
 		}
 
 	return (0);
