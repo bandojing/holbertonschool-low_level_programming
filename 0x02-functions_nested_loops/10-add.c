14229b7ccaa8abfffe23b42a16c30efa4e98e286
bandojing <technofrosty@yahoo.com>
2020-02-10 21:12:56 +0000

    a function that adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * int add - adds two integers and outputs the sum
+ *
+(* a blank line
+* Desription: Longer description of the function)?
+(* section header: Section description)*
+* Return: returns an integer named three
+*/
+
+int add (int one,int two)
+{
+	int three;
+	three = one + two;
+	return three;;
+}
+
