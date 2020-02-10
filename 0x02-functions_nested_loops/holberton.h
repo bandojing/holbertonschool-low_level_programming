31493ea1a0f1753556a3915e18711f3790997076
bandojing <technofrosty@yahoo.com>
2020-02-10 21:13:50 +0000

    header file for 0x02 with all function prototypes

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,21 @@
+/* 0*/
+void print_alphabet_x10(void);
+/*01*/
+void print_alphabet(void);
+/*02*/
+int _islower(int c);
+/*03*/
+int _isalpha(int c);
+/*05*/
+int print_sign(int n);
+
+/*07*/
+int print_last_digit(int);
+
+int _putchar(char c);
+
+/*10*/
+int add(int, int);
+
+/*11*/
+void print_to_98(int n);
