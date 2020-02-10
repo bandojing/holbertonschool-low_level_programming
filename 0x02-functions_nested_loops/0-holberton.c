cd136a762616aa850a45f082db22c40cf1e2af73
bandojing <technofrosty@yahoo.com>
2020-02-10 16:14:15 +0000

    a program that prints Holberton, followed by a new line

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - uses putchar to print out Holberton in ascii
+(* a blank line
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: returns 0
+*/
+
+int main(void)
+{
+
+	putchar(72);
+	putchar(111);
+	putchar(108);
+	putchar(98);
+	putchar(101);
+	putchar(114);
+	putchar(116);
+	putchar(111);
+	putchar(110);
+
+
+	return (0);
+}
