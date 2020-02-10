66ce25753f094ce9644f4c618233177587a3cf74
bandojing <technofrosty@yahoo.com>
2020-02-10 21:12:14 +0000

    a function that prints the last digit of a number.

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit  - Short description, single line
+ *
+(* a blank line
+* Description: finds the last digit of an integer
+(* section header: Section description)*
+* Return: returns last digit which equals int%10
+*/
+
+int print_last_digit(int)
+{
+	int last;
+
+	last = int % 10;
+
+	return ( last );
+}
