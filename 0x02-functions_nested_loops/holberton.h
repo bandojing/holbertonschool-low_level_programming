d9e0733a2a3efecb8b6a755681da9f03ad7d973f
bandojing <technofrosty@yahoo.com>
2020-02-11 14:38:40 +0000

    funtion prototypes for 0x02 functions nested loops

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,21 +1,12 @@
-/* 0*/
 void print_alphabet_x10(void);
-/*01*/
 void print_alphabet(void);
-/*02*/
 int _islower(int c);
-/*03*/
 int _isalpha(int c);
-/*05*/
 int print_sign(int n);
-
-/*07*/
+int _abs(int);
 int print_last_digit(int);
-
+void jack_bauer(void);
+void times_table(void);
 int _putchar(char c);
-
-/*10*/
 int add(int, int);
-
-/*11*/
 void print_to_98(int n);
