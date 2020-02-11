e562e0cd75cc44022aae99a575ec35ce5371debd
bandojing <technofrosty@yahoo.com>
2020-02-11 14:33:05 +0000

    a program that prints Holberton, followed by a new line

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -10,16 +10,6 @@
 int main(void)
 {
 
-	putchar(72);
-	putchar(111);
-	putchar(108);
-	putchar(98);
-	putchar(101);
-	putchar(114);
-	putchar(116);
-	putchar(111);
-	putchar(110);
-
-
+	printf("Holberton\n");
 	return (0);
 }
