29fcbaf792b5f921a69d582c7d4f5eb87a7471cc
bandojing <technofrosty@yahoo.com>
2020-02-11 18:22:13 +0000

    a function that prints the last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,13 +4,29 @@
  * print_last_digit  - Short description, single line
  *
 (* a blank line
-* @last: last is integer passed into the function
+* @ablast: last is integer passed into the function
 (* section header: Section description)*
 * Return: returns last digit which equals int%10
 */
 
-int print_last_digit(int last)
+int print_last_digit(int ablast)
 {
 
-	return (last % 10);
+
+	if (ablast < 0)
+	{
+		return((ablast % 10) * -1);
+
+		_putchar(ablast + '0');
+
+	}
+	else if (ablast >= 0)
+	{
+		return((ablast % 10));
+		_putchar(ablast + '0');
+	}
+
+
+	return (0);
+
 }
