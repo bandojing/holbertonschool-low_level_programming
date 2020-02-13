780f9c9ea4690cf63e8e7b1b9e16e64022df60bf
bandojing <technofrosty@yahoo.com>
2020-02-13 20:08:48 +0000

    a function that draws a diagonal line on the terminal

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -10,6 +10,7 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
+void print_diagonal(int n);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
