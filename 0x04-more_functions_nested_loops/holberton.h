6c5659e12644e9f6d3cc90ff5247d4cc80cd6b40
bandojing <technofrosty@yahoo.com>
2020-02-13 15:33:40 +0000

    holberton header file containing function prototypes

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,8 +1,12 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 #include <stdio.h>
+int _putchar(char c);
 void positive_or_negative(int i);
 int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
