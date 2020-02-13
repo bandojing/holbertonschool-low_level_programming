c6f41088b5cca0112e4b05bc067c529c1e319f28
bandojing <technofrosty@yahoo.com>
2020-02-13 16:19:47 +0000

    a function that prints the numbers, from 0 to 9, followed by a new line

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - this function prints out 0-9 skipping over 2 and 4
+(*
+* Description: prints out numbers 0-9 skipping 2 and 4
+(*
+* Return: returns void
+*/
+
+void print_most_numbers(void)
+{
+
+	int numb;
+
+	for (numb = 48; numb <= 57; numb++)
+	{
+		if (numb == 50 || numb == 52)
+		{
+		}
+		else
+			_putchar(numb);
+
+	}
+}
