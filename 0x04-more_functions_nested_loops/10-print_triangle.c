e63d53a2359d796f0174df3a94a03025c23c2155
bandojing <technofrosty@yahoo.com>
2020-02-14 20:57:09 +0000

    a function that prints a triangle, followed by a new line

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_triangle - size determines size of triangle printed with #
+ * @size: size determines size of triangle
+(* a blank line
+* Description: size determines size of triangle
+* Return: returns voie
+*/
+
+
+void print_triangle(int size)
+{
+
+	int printspaces;
+	int row;
+	int printhash;
+
+
+	if (size > 0)
+	{
+		for (row = 1; row <= size; row++)
+		{
+			for (printspaces = size; printspaces >= row; printspaces--)
+			{
+				_putchar(32);
+			}
+			for (printhash = 1; printhash <= row; printhash++)
+			{
+				_putchar(35);
+			}
+			_putchar(10);
+		}
+	}
+}
