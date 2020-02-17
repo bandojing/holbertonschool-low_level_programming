1fb6af4de35ac334babb86fc46559456d696c7e6
bandojing <technofrosty@yahoo.com>
2020-02-17 16:18:00 +0000

    a function that prints a string, followed by a new line, to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _puts - prints a string then a newline takes in a pointer to a string
+ * @str: the string to be printed
+(* a blank line
+* Description: prints a string followed by a newline
+(* section header: Section description)*
+* Return: returns void
+*/
+
+void _puts(char *str)
+{
+
+	int stringcounter;
+
+	for (stringcounter = 0; str[stringcounter] != '\0'; stringcounter++)
+	{
+		printf("%c", str[stringcounter]);
+	}
+	printf("\n");
+}
