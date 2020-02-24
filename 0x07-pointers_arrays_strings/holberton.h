45e91621c0b38dca4d99a22f18f2f4b593a261cf
bandojing <technofrosty@yahoo.com>
2020-02-24 20:57:25 +0000

    added all the remaining prototypes for 0x07 pointers arrays and strings

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -25,6 +25,8 @@ void print_chessboard(char (*a)[8]);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
+void set_string(char **s, char *to);
+void print_diagsums(int *a, int size);
 unsigned int _strspn(char *s, char *accept);
 #endif /* HOLBERTON_H */
 
