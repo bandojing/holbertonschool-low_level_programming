5523a8e0777f34660ee46795b3f02407453b9f85
bandojing <technofrosty@yahoo.com>
2020-03-02 14:52:53 +0000

    removed comment blocks, added single line comments, made betty compliant

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -16,63 +16,32 @@ int main(int argc, char *argv[])
 	int sum = 0;
 	int argarray;
 	int argarray_2;
-
+	/*if the number of arguments is equal to one*/
 	if (argc < 2)
 	{
 		printf("0\n");
-		return(0);
+		return (0);
 	}
-
-
-
-/**
- *	if (isdigit(argv[nume if (argc > 1)
- *			    {
- *				    for (numbersentered = 0; numbersentered < argc; numbersentered++)
- *				    {
- *					    sum += atoi(argv[numbersentered]);
- *				    }
- *				    printf("%d\n", sum);
- *
- *				    return (0);rsentered]) == 0)
- *		printf("0\n");
- */
+	/* check to see if any of the arguments in the argv array contain*/
+	/*symbols that are not numbers*/
 	for (argarray = 1; argarray < argc; argarray++)
 	{
-		for(argarray_2 = 0; argv[argarray][argarray_2]; argarray_2++)
+		for (argarray_2 = 0; argv[argarray][argarray_2]; argarray_2++)
 		{
 			if (argv[argarray][argarray_2] < 48 || argv[argarray][argarray_2] > 57)
 			{
 				printf("Error\n");
-				return(-1);
+				return (-1);
 			}
 		}
 	}
-
+	/*remembert to start from 1 argv[0] is the call to the program ./4add*/
+	/*adds all of the numbers entered as arguments and prints the sum*/
 	for (numbersentered = 1; numbersentered < argc; numbersentered++)
-/**
- *	if (argc > 1)
- *		for (numbersentered = 0; numbersentered < argc; numbersentered++)
- *		{
- *			if (isalpha(argv[numbersentered]))
- *			    printf("Error\n");
- *			    return (-1);
- */			sum += atoi(argv[numbersentered]);
+		sum += atoi(argv[numbersentered]);
 
 		printf("%i\n", sum);
 
 		return (0);
-/**
- *	for (numbersentered = 0; numbersentered < argc; numbersentered++)
- *	{
- *		if (argv[numbersentered] < 48 || argv[numbersentered] > 57)
- *			printf("Error\n");
- *			return (1);
- *		}
- *
- *		sum += atoi(argv[numbersentered]);
- *	}
- *	printf("%i", sum);
- */
-	return (0);
+
 }
