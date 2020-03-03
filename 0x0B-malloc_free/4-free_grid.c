3ace7e8f1201a5bcc683c12fb8203c3f08be3728
bandojing <technofrosty@yahoo.com>
2020-03-03 19:38:18 +0000

    initial commit

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * strdup - copies one string to another and returns pointer to copy
+ * @grid: string passed in from outside will be copied into newstring
+ * @height: height
+(* a blank line
+* Description:  a function that returns a pointer to a newly allocated
+* space in memory, which contains a copy of the string given as a parameter
+(* section header: Section description)*
+* Return: returns copy of str
+*/
+
+void free_grid(int **grid, int height)
+{
+
+}
