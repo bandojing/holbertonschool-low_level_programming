db43255cbcd3f2135c2a6ac59500cae656bb4cd1
bandojing <technofrosty@yahoo.com>
2020-03-03 19:35:56 +0000

    initial commit

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * alloc_grid - copies one string to another and returns pointer to copy
+ * @width: string passed in from outside will be copied into newstring
+ * @height: height
+(* a blank line
+* Description:  a function that returns a pointer to a newly allocated
+* space in memory, which contains a copy of the string given as a parameter
+(* section header: Section description)*
+* Return: returns copy of str
+*/
+
+int **alloc_grid(int width, int height)
+{
+	int *array;
+	return (array);
+}
