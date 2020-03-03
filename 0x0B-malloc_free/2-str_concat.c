8bed4f4439762fe427f9627b47386e2c416a42e3
bandojing <technofrosty@yahoo.com>
2020-03-03 19:33:11 +0000

    initial commit

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * strdup - copies one string to another and returns pointer to copy
+ * @s1: string passed in from outside will be copied into newstring
+ * @s2: second string
+(* a blank line
+* Description:  a function that returns a pointer to a newly allocated
+* space in memory, which contains a copy of the string given as a parameter
+(* section header: Section description)*
+* Return: returns copy of str
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+
+	return(0);
+}
