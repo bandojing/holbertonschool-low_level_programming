72ebc75653a1ecbd83b06e39fabb0d3050244a1f
bandojing <technofrosty@yahoo.com>
2020-03-05 19:40:00 +0000

    re-edited changed newstring[size2] to newstring[size1]

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,9 +14,8 @@
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int leng1;
-	unsigned int leng2;
-	/*unsigned int sumleng;*/
+	unsigned int leng1 = 0;
+	unsigned int leng2 = 0;
 	char *newstring;
 	unsigned int size1;
 	unsigned int size2;
@@ -33,7 +32,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		if (n >= leng2)
 			n = leng2;
 
-		newstring = malloc((leng1 + leng2) * sizeof(char));
+		newstring = malloc((leng1 + n) * sizeof(char));
 		if (newstring == NULL)
 			return (NULL);
 
@@ -42,12 +41,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			newstring[size1] = s1[size2];
 		}
 
-				for (size2 = 0; size2 < leng2; size1++, size2++)
+				for (size2 = 0; size2 < n; size1++, size2++)
 				{
 					newstring[size1] = s2[size2];
 				}
 
-				newstring[size2] = '\0';
+				newstring[size1] = '\0';
 
 	return (newstring);
 
