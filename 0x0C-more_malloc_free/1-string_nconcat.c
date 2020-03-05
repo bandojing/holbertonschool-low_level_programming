ae35a7346d2d15e703fb9980e264b457026a5f90
bandojing <technofrosty@yahoo.com>
2020-03-05 19:05:36 +0000

    after n >= leng2 added n=leng2

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,67 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+/**
+ * string_nconcat - a function that concactenates two strings
+ * @s1: first string passed in
+ * @s2: second string passed in
+(* a blank line
+* Description: a function that concatenates two strings
+(* section header: Section description)*
+* Return: returns newstring that contains
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+        unsigned int leng1;
+	unsigned int leng2;
+	/*unsigned int sumleng;*/
+	char *newstring;
+	unsigned int size1;
+	unsigned int size2;
+
+	if (s1 != NULL)
+	for (leng1 = 0; s1[leng1] != '\0'; leng1++)
+		;
+
+
+	if (s2 != NULL)
+		for (leng2 = 0; s2[leng2] != '\0'; leng2++)
+			;
+
+	/*newstring = malloc((leng1 + leng2) * sizeof(char));*/
+	/*if (newstring == NULL)*/
+	/*return (NULL);*/
+
+	/*if s1 is not empty copy it to newstring*/
+	/*if (s1 != NULL)*/
+/**
+ *	for( size1 = 0, size2 = 0; size2 < leng1; size1++, size2++)
+ *	{
+ *		newstring[size1] = s1[size2];
+ *
+ *}	if (s2 != NULL)
+ */
+		if (n >= leng2)
+			n = leng2;
+
+		newstring = malloc((leng1 + leng2) * sizeof(char));
+		if (newstring == NULL)
+			return (NULL);
+
+		for (size1 = 0, size2 = 0; size2 < leng1; size1++, size2++)
+		{
+			newstring[size1] = s1[size2];
+		}
+
+				for (size2 = 0; size2 < leng2; size1++, size2++)
+				{
+					newstring[size1] = s2[size2];
+				}
+
+				newstring[size2] = '\0';
+
+	return (newstring);
+
+
+}
