d071d258c974ecf5a0f3e30d4f6bd0af4f075b8d
bandojing <technofrosty@yahoo.com>
2020-03-05 14:54:02 +0000

    first commit using exit(98) normal process termination with a status value of 98

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked -  a function that allocates memory using malloc
+ * @b: an unsigned int passed into the function
+(* a blank line
+* Description: a function that allocates memory using malloc
+(* section header: Section description)*
+* Return: pointer to assigned memory
+*/
+void *malloc_checked(unsigned int b)
+{
+
+	unsigned int *mem;
+
+	mem = malloc(b * sizedof(unsigned int));
+	if (mem == NULL)
+		exit(98);
+
+	return (mem);
+}
