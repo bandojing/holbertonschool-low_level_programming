aad43e22fab952368bbcbd21fe23f62fa6190721
bandojing <technofrosty@yahoo.com>
2020-03-05 01:09:34 +0000

    corrected with nested for loops

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,11 +9,40 @@
 * Description:  a function that returns a pointer to a newly allocated
 * space in memory, which contains a copy of the string given as a parameter
 (* section header: Section description)*
-* Return: returns copy of str
+* Return: returns pointer to 2d array filled with zeroes
 */
 
 int **alloc_grid(int width, int height)
 {
-	int *array;
-	return (array);
+  int rows;
+  int cols;
+  int **gridmemory;
+
+  /*first case if width and height are 0 or negative return null*/
+  if (width <= 0 || height <= 0)
+    return(NULL);
+
+  /*allocate memory based on the height aka rows*/
+  gridmemory = malloc(height * sizeof(int));
+  if (gridmemory == NULL)
+    return(NULL);
+
+  /*nested for loop to build a grid from 2d array*/
+  for (rows = 0; rows < height; rows++)
+    {
+      /*checks if any of the rows are null*/
+      gridmemmory[rows] = malloc(width * sizeof(int));
+      if (gridmemory[rows] == NULL);
+      return(NULL);
+
+      /*for loop to fill in grid space with zeroes*/
+      for(cols = 0; cols < width; cols++)
+	{
+	  gridmemory[rows][cols] = 0;
+	}
+    }
+
+  /*returns the pointer to the grid array filled with zeroes*/
+  return (gridmememory);
+
 }
