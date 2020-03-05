0cf6d452f1ff18328da071267dbe1a9a4b67f644
bandojing <technofrosty@yahoo.com>
2020-03-05 14:30:58 +0000

    pushing before merge

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,13 +9,51 @@
 * Description:  a function that returns a pointer to a newly allocated
 * space in memory, which contains a copy of the string given as a parameter
 (* section header: Section description)*
-* Return: returns copy of str
+* Return: returns pointer to 2d array filled with zeroes
 */
 
 int **alloc_grid(int width, int height)
 {
 
-	if (width || height == 0)
-		return (NULL);
+	int rows;
+	int cols;
+	int **grid;
 
-}
+	/*first case if width and height are 0 or negative return null*/
+	if (width <= 0 || height <= 0)
+		return(NULL);
+
+	/*allocate memory based on the height aka rows*/
+	grid = malloc(height * sizeof(int));
+	if (grid == NULL)
+		return(NULL);
+
+	/*if rows null frees up memory*/
+	for (rows = 0; rows < height; rows++)
+	{
+		/*checks if any of the rows are null*/
+		grid[rows] = malloc(width * sizeof(int));
+		if (grid[rows] == NULL)
+		{
+			for (cols = 0; cols < rows; cols++)
+				free(grid[cols]);
+			free(grid);
+			return (NULL);
+		}
+	}
+
+	for (rows = 0; rows < height; rows++)
+	{
+
+		/*for loop to fill in grid space with zeroes*/
+		for (cols = 0; cols < width; cols++)
+		{
+			grid[rows][cols] = 0;
+		}
+	}
+
+	/*returns the pointer to the grid array filled with zeroes*/
+	return (grid);
+
+
+}}
