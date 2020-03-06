62232572ecf9874d14ce3f76392740e59302226a
bandojing <technofrosty@yahoo.com>
2020-03-06 17:30:57 +0000

    added +1 to newstring malloc statement

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,7 +32,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		if (n >= leng2)
 			n = leng2;
 
-		newstring = malloc((leng1 + n) * sizeof(char));
+		newstring = malloc((leng1 + n + 1) * sizeof(char));
 		if (newstring == NULL)
 			return (NULL);
 
