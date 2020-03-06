a8948fda72eb6e35abba734d7325cf4f112a27e6
bandojing <technofrosty@yahoo.com>
2020-03-06 21:34:20 +0000

    made betty compliant and added int * on line 27

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,39 +20,37 @@ int **alloc_grid(int width, int height)
 	int **grid;
 
   /*first case if width and height are 0 or negative return null*/
-  if (width <= 0 || height <= 0)
-    return(NULL);
+	if (width <= 0 || height <= 0)
+		return (NULL);
 
-  /*allocate memory based on the height aka rows*/
-  grid = malloc(height * sizeof(int));
-  if (grid == NULL)
-    return(NULL);
+	/*allocate memory based on the height aka rows*/
+	grid = malloc(height * sizeof(int *));
+	if (grid == NULL)
+		return (NULL);
 
   /*if rows null frees up memory*/
-  for (rows = 0; rows < height; rows++)
-    {
+	for (rows = 0; rows < height; rows++)
+	{
       /*checks if any of the rows are null*/
-      grid[rows] = malloc(width * sizeof(int));
-      if (grid[rows] == NULL)
-	{
-	  for (cols = 0; cols < rows; cols++)
-	    free(grid[cols]);
-	  free(grid);
-	  return (NULL);
+		grid[rows] = malloc(width * sizeof(int));
+		if (grid[rows] == NULL)
+		{
+			for (cols = 0; cols < rows; cols++)
+				free(grid[cols]);
+			free(grid);
+			return (NULL);
+		}
 	}
-    }
-
-  for (rows = 0; rows < height; rows++)
-    {
-
-      /*for loop to fill in grid space with zeroes*/
-      for (cols = 0; cols < width; cols++)
+	for (rows = 0; rows < height; rows++)
 	{
-	  grid[rows][cols] = 0;
+/*for loop to fill in grid space with zeroes*/
+		for (cols = 0; cols < width; cols++)
+		{
+			grid[rows][cols] = 0;
+		}
 	}
-    }
 
   /*returns the pointer to the grid array filled with zeroes*/
-  return (grid);
+	return (grid);
 
 }
