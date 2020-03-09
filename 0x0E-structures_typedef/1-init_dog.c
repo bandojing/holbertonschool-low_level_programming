f3e6454bbf63f51f34d541ebc2d0907dabd158be
bandojing <technofrosty@yahoo.com>
2020-03-09 19:18:04 +0000

    removed holberton header from include

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,3 @@
-#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
@@ -16,7 +15,7 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
-		return ();
+		return;
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
