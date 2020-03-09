5a9b57f698156a09e39f8c796e39e7978a8773a4
bandojing <technofrosty@yahoo.com>
2020-03-09 18:11:21 +0000

    edited comments and removed a header include

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
-#include "holberton.h"
 #include <stdlib.h>
 /**
  * main - returns the name of the file it was compiled from
- * @void: this function has nothing passed to it
-(* a blank line
 * Description: prints the name of the file it was compiled from
-(* section header: Section description)*
 * Return: prints the name of the file it was compiled from
 */
 
