309a9149fb421e99dc1c101c4426bd589e35070e
bandojing <technofrosty@yahoo.com>
2020-03-09 20:14:11 +0000

    added new struct called dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,6 +14,14 @@ char *name;
 float age;
 char *owner;
 };
+
+struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
