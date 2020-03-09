d36d2f4831fe7940a705a05c4f8a1ffecda737a4
bandojing <technofrosty@yahoo.com>
2020-03-09 18:52:50 +0000

    initial commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * init_dog - a function that initialize a variable of type struct dog
+ * @d: pointer to name of structure dog
+ * @name: pointer to name member of struct dog
+ * @age: age member of struct dog
+ * @owner: pointer to owner member of dog
+(* a blank line
+* Description: a function that initialize a variable of type struct dog
+(* section header: Section description)*
+* Return: returns void
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+
+/**
+ * struct dog - struct with three members
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: struct dog a structure with members n\
+ame age owner
+*/
+struct dog
+{
+        char *name;
+        float age;
+        char *owner;
+};
+
+
+
+}
