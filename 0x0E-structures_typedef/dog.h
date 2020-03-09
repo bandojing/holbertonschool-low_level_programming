87d21da8345aeb482b813d1e5c2944e96685090d
bandojing <technofrosty@yahoo.com>
2020-03-09 18:17:16 +0000

    a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+int _putchar(char c);
+/**
+ * struct dog - struct with three members
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: struct dog a structure with members name age owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
