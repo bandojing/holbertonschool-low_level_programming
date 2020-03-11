e6ee1e98cbb06b427076373f2fc550fe0eca374e
bandojing <technofrosty@yahoo.com>
2020-03-11 17:08:20 +0000

    added stdio, stdlib, stddef, and function_pointers header files

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include "function_pointers.h"
 /**
  * print_name - a function that prints a name
  * @name: name passed into the printname function
