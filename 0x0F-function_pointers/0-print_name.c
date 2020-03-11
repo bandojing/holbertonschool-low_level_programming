fd58e5e69a8f9c1f039495f253fe724ba1d50a1b
bandojing <technofrosty@yahoo.com>
2020-03-11 15:09:43 +0000

    working out pointers to functions

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * print_name - a function that prints a name
+ * @name: name passed into the printname function
+ * @f: pointer to function
+(* a blank line
+* Description: a function that prints a name.
+(* section header: Section description)*
+* Return: return void
+*/
+void print_name(char *name, void (*f)(char *))
+{
+f = &print_name;
+}
