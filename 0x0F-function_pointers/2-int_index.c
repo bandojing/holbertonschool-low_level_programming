ad6773e450dd037c0975b3c8cd55fb4768e493b6
bandojing <technofrosty@yahoo.com>
2020-03-11 18:02:24 +0000

    changed first comparison to array == NULL || cmp == NULL, always check if pointers are null

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -17,7 +17,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int array_index = 0;
 
-	if (array != NULL || cmp != NULL)
+	/*if (array != NULL || cmp != NULL)*/
+	if (array == NULL || cmp == NULL)
+		return (-1);
 
 	if (size <= 0)
 		return (-1);
