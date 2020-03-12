07cd99add875747192d2e6ff360d6b5a30169e70
bandojing <technofrosty@yahoo.com>
2020-03-12 18:21:07 +0000

    contains struct with while loop used to select an operation

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,3 +1,17 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include "calc.h"
+/**
+ * get_op_func -  the function that selects the correct function to
+ * perform the operation asked by the user
+ * @s: the string passed in from the user
+(* a blank line
+* Description: function that selects a function to perform the operation
+* asked by the user
+(* section header: Section description)*
+* Return: returns an integer
+*/
 int (*get_op_func(char *s))(int, int)
 {
 
@@ -9,12 +23,15 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i;
+
+	int struct_counter = 0;
 
 
-	while (ops[i]->f != NULL)
+	while (struct_counter < 5)
 	{
 
-		if (s
+		if (*ops[struct_counter] == *s && s[struct_counter])
+			return (ops[struct_counter].f);
+		struct_counter++;
 	}
 }
