fe523c836e76a63873eaadcb38168f02b9c69659
bandojing <technofrosty@yahoo.com>
2020-03-14 22:05:08 +0000

    made betty compliant

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -33,7 +33,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			/*if ( n == '\0')*/
 			printf("(nil)");
 
-		if ( copy /*va_arg(list, char *)*/ != NULL)
+		if (copy /*va_arg(list, char *)*/ != NULL)
 			printf("%s", copy /*va_arg(list, char *)*/);
 
 		if (separator != NULL && argloop <  n - 1)
