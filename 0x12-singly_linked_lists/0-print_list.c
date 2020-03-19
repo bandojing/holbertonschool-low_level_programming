2330c5b27efac7f572caa6911c68e05cb811ce28
bandojing <technofrosty@yahoo.com>
2020-03-19 23:38:46 +0000

    initial commit printing all elements of a linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+/**
+ * print_list - prints all the elements of list_t list
+ * @h: pointer to struct passed into function
+(* a blank line
+* Description: prints the len and str members of struct
+* as well as the number of elements
+(* section header: Section description)*
+* Return: returns number of elements
+*/
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0](nil)");
+		else
+			printf("[%d]%s\n", h->len, h->str);
+		h = h->next;
+		count++;
+		printf("-> %d elements", count);
+	}
+}
