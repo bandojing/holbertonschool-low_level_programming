864354e2b4fc54b33c50e272a9b5f54eedc59c0a
bandojing <technofrosty@yahoo.com>
2020-03-20 01:26:22 +0000

    added  \n at printf 0 nil

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
  * print_list - prints all the elements of list_t list
  * @h: pointer to struct passed into function
@@ -15,7 +16,7 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 		{
-			printf("[0] (nil)");
+			printf("[0] (nil)\n");
 		}
 		else
 			printf("[%u] %s\n", h->len, h->str);
