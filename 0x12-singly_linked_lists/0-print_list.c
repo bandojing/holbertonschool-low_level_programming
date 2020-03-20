424d871a16095bab535656476c442cfc1cd13803
bandojing <technofrosty@yahoo.com>
2020-03-20 01:05:32 +0000

    added brackets around first printf

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,10 +14,14 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
+		{
 			printf("[0] (nil)");
+		}
 		else
 			printf("[%u] %s\n", h->len, h->str);
+
 		h = h->next;
+
 		count++;
 	}
 	return(count);
