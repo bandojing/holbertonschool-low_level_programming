dc1856f35e04e34ca56a1bdbdd29813db935cad6
bandojing <technofrosty@yahoo.com>
2020-03-20 01:53:07 +0000

    cast membercount as size_t added lists.h to top of file

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,3 +1,4 @@
+#include "lists.h"
 /**
  * list_len - returns number of elements in a linked list
  * @h: pointer to struct
@@ -8,7 +9,7 @@
 */
 size_t list_len(const list_t *h)
 {
-	int memberamount = 0;
+	size_t  membercount = 0;
 	list_t *headcount = head;
 
 	while (headcount != NULL)
