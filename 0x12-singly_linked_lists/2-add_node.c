81729ab31f62ce9a900083b949fc2ef50e6324f5
bandojing <technofrosty@yahoo.com>
2020-03-21 04:22:02 +0000

    added malloc checks for null, added members in order they are in in the struct, tried to create blocks of code for readability

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,20 +13,32 @@ list_t *add_node(list_t **head, const char *str)
 {
 
 	list_t *pnewnode;
-	char *duplicatestring = strdup(str);
+	char *duplistring = strdup(str);
 	int stringlength = 0;
 
-	pnewnode = malloc(sizeof(list_t);
+	pnewnode = malloc(sizeof(list_t));
 
 	if (pnewnode == NULL || str == NULL)
 	return (NULL);
 
 	while (str[stringlength] != '\0')
-		stringlength++;
+		++stringlength;
 
+	pnewnode->str = duplistring;
 	pnewnode->len = stringlength;
-	pnewnode->str = duplicatestring;
 	pnewnode->next = *head;
 	*head = pnewnode;
+
 	return (pnewnode);
+
 }
+/*create variables*/
+/*malloc a new struct*/
+/*if the new struct/node is null or if str is null return null*/
+/*while string is there increment the stringlength*/
+/*the len member = the string length*/
+/*the string member equals the string passed in*/
+/*the next pointer points to the head*/
+/*the newnode now becomes the head*/
+/*the main function calls the print_list function made earlier*/
+/*print_list function is number 0*/
