9ca14685d93f1bd7f849b3c9634bbae9af3324bd
bandojing <technofrosty@yahoo.com>
2020-03-21 19:25:56 +0000

    made betty compliant by adding comments and changed name to void calledFirst(void)

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 void __attribute__ ((constructor)) calledFirst();
-
-void calledFirst()
+/**
+ * calledFirst - prints before main exits
+ * @void: accepts nothing as an argument
+ (* a blank line
+ * Description: uses __attribute__((constructor)) calledFirst();
+ * this allows for this function to print before main exits
+ (* section header: Section description)*
+ * Return: returns void
+ */
+void calledFirst(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
 
