401ba34a09bfd7bd5ef0d6e7767e3071bf95a331
bandojing <technofrosty@yahoo.com>
2020-03-21 19:16:59 +0000

    function that prints before main exits, included stdio.h and added void__attribute__((constructor))clear

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,4 +1,7 @@
-void tortoiseandhare(void) __attribute__ ((constructor))
+#include <stdio.h>
+void __attribute__ ((constructor)) calledFirst();
+
+void calledFirst()
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
