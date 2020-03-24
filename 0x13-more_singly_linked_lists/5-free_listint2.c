a13c4039c6593f24a5d15aed3f7b8fd8c458e80f
bandojing <technofrosty@yahoo.com>
2020-03-24 21:07:12 +0000

    compiles but there is a segmentation fault

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+  * free_listint2 - free up a linked list set head to null
+  * @head: a double pointer to the head
+  *
+  * Description: funtion that delets a linked list and sets
+  * the *head to null
+  * Returns: returns void
+  */
+void free_listint2(listint_t **head)
+{
+	listint_t *temporary;
+
+	while (head != NULL)
+	{
+		temporary = *head;
+		(*head) = (*head)->next;
+		/*free(temporary->n);*/
+		free(temporary);
+	}
+
+	/*head == NULL;*/
+}
+/*create a temporary struct*/
+/* while *head not equal to null*/
+/* temporary is a copy of *head */
+/*free the n member of the struct in the list*/
+/*free the whole node*/
+/*set the *head to null*/
