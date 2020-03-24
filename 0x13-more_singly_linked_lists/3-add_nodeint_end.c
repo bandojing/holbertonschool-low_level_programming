8b7ca2f2c728f2b4be0d1fe1fb1e1dc394e39ff7
bandojing <technofrosty@yahoo.com>
2020-03-24 17:34:17 +0000

    error: invalid type argument of ‘->’ (have ‘listint_t’)    swap = swap->next;

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,26 +10,28 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *swap;
+
 	listint_t *node_to_add;
-	int newn = n;
 
 	node_to_add = malloc(sizeof(listint_t));
-			if (node_to_add == NULL)
+		if (node_to_add == NULL);
 			return (NULL);
-	
-	swap = *head;
 
-		node_to_add->n = newn;
-		node_to_add->next = NULL;
-		swap = *head;
+	listint_t swap = *head;
 
+	node_to_add->n = n;
+	node_to_add->next = NULL;
+
+	if (*head == NULL)
+		*head = node_to_add;
 
 	while (swap->next != NULL)
 		swap = swap->next;
 
 	swap->next = node_to_add;
 
-		return (node_to_add);
+	return (node_to_add);
+
+
 
 }
