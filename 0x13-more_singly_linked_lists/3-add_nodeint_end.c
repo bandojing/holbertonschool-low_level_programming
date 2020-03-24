3ce7ff37ff3f1562521a37055ea27bf73879cc4f
bandojing <technofrosty@yahoo.com>
2020-03-24 18:02:24 +0000

    added if else while loops and deleted ; after malloc

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -5,30 +5,37 @@
   * @n: const int passed into function
   * Description: uses while loop to increment var that holds
   * number of elements in a linked list
-  * Return: number of elements in the linked list
+  * Return: returns the new list
   */
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 
 	listint_t *node_to_add;
+	listint_t *swap;
 
 	node_to_add = malloc(sizeof(listint_t));
-		if (node_to_add == NULL);
+		if (node_to_add == NULL)
 			return (NULL);
 
-	listint_t swap = *head;
 
 	node_to_add->n = n;
 	node_to_add->next = NULL;
 
 	if (*head == NULL)
+	{
 		*head = node_to_add;
+	}
+	else
+	{
+		swap = *head;
 
 	while (swap->next != NULL)
 		swap = swap->next;
 
 	swap->next = node_to_add;
+	}
+
 
 	return (node_to_add);
 
