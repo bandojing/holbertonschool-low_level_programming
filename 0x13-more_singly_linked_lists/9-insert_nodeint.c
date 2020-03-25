0a9a02142d6b4ba8d6afd0f1768f3cc8419598eb
bandojing <technofrosty@yahoo.com>
2020-03-25 17:52:54 +0000

    changed idx back to idx - 2

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	swapspace = (*head);
 
-	for (trav = 0; trav < idx; trav++)
+	for (trav = 0; trav < idx - 2; trav++)
 	{
 		swapspace = swapspace->next;
 	}
