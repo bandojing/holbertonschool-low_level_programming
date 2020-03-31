3b3b24cd757d944e5286a2c1f69b0e6a4b97e8d6
bandojing <technofrosty@yahoo.com>
2020-03-31 20:31:25 +0000

    betty compliant, added O_TRUNc and found length of string for write function

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,31 +1,45 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fontl.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include "holberton.h"
-
-
+/**
+ * create_file - creates a file and adds text
+ * @filename: name of file to open
+ * @text_content: text to add to open file
+(* a blank line
+ * Description: adds text to an open file
+ * Return: returns -1 if failure 1 if successful
+ */
 int create_file(const char *filename, char *text_content)
 {
-	int file;
+	int fd;
+	int w;
+	int len = 0;
+
 
 	if (filename == NULL)
-		return (
+		return (-1);
 
-	fd = open(filename, O_CREAT, 'rw');
+	/*get length of text_content*/
+		while (text_content != '\0')
+		{
+			len++;
+		}
+
+
+	/*truncate flag ,*/
+	fd = open(filename, O_CREAT, O_TRUNC, 0600);
 
 	if (fd == -1)
 	{
 		return (-1);
 	}
 
-	
-	write (fd, text_content);
+/*where writing to, string writing to, length*/
 
-	if (write (fd, text_content) == -1)
+	w = write(fd, text_content, len);
+
+	if (w == -1)
+	{
 		return (-1);
+	}
 
 	close(fd);
 
