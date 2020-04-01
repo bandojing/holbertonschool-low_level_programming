7a470a04ea79ee492b7f68b7af3dc98f79e85780
bandojing <technofrosty@yahoo.com>
2020-04-01 02:20:27 +0000

    added test for if text is null writes nothing to the file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,12 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-
+	
+	if (text_content == NULL)
+	{
+		w = write(fd, text_content, 0);
+		return (1);
+	}
 
 	for (counter = 0; text_content[counter] != '\0'; counter++)
 	{
