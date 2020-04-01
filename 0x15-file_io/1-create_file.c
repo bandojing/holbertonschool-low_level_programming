6e34f69caed99a959da4fdb021644730b328de0e
bandojing <technofrosty@yahoo.com>
2020-04-01 02:44:17 +0000

    made betty compliant removed space before open ) on line 20

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, mode )/* 0600)*/;
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, mode)/* 0600)*/;
 
 	if (fd == -1)
 	{
