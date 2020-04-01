0d645887de16bb06a4cba1c6c77fa2f52c51f867
bandojing <technofrosty@yahoo.com>
2020-04-01 02:42:28 +0000

    added O_TRUNC and used mode_t mode for permissions

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,12 +12,12 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	int w;
 	int counter;
-
+	mode_t mode = S_IRUSR | S_IWUSR;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, mode )/* 0600)*/;
 
 	if (fd == -1)
 	{
