82b51bc24f3b975541cc35b40844372a06b827ef
bandojing <technofrosty@yahoo.com>
2020-04-01 02:33:20 +0000

    w writes to empty file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 	{
-		w = write(fd, text_content, 0);
+		w = write(fd, "", 0);/*, text_content, 0);*/
 		if (w == -1)
 			return (-1);
 
