a2eb3b7223f9c0df7d80a3bac9fc5f2411479818
bandojing <technofrosty@yahoo.com>
2020-04-02 03:50:40 +0000

    changed char* to char * and added blank line after declarations

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * get_endiannes - checks the endiannes of the system you are using
+ * get_endianness - checks the endiannes of the system you are using
 *
 * Description: checks the endiannes of the systme you are using
 * Return: returns 1 if little endian 0 if big endian
@@ -9,6 +9,7 @@
 int get_endianness(void)
 {
 	unsigned int determinant = 1;
-	char *endianness = (char*) &determinant;
-	return (int)*endianness;
+	char *endianness = (char *) &determinant;
+
+	return ((int)*endianness);
 }
