6206c8029efd6156fea561663659a6ca9d44cf07
bandojing <technofrosty@yahoo.com>
2020-04-02 02:58:39 +0000

    changed write 2 to write 1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,8 +10,8 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful", 31);
-	write(2, "- Dora Korpar, 2015-10-19\"\n", 27);
+	write(1, "and that piece of art is useful", 31);
+	write(1, "- Dora Korpar, 2015-10-19\"\n", 27);
 
 	return (1);
 }
