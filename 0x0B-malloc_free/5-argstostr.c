b5291630561e43a4df702fb374ad2277be26378b
bandojing <technofrosty@yahoo.com>
2020-04-04 01:55:28 +0000

    goes into infinite loop

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -13,16 +13,35 @@
 char *argstostr(int ac, char **av)
 {
 	int counter;
+	char **string_return;
+	int stringlen;
+	int counter1;
 
 	if (ac == 0 || *av == NULL)
 	{
 		return (NULL);
 	}
+	
+	while (av != NULL)
+	{
+		stringlen++;
+	}
+	
+	string_return = malloc((stringlen/* + 1*/) *(sizeof(char)));
+	if (string_return == NULL)
+	{
+		return (NULL);
+	}
+
+	for (counter1 = 0; counter1 < (stringlen/* + 1*/); counter1++)
+	{
+		string_return[counter1] = av[counter1];
+	}
 
 	for (counter = 0; counter < ac; counter++)
 	{
-		printf("%s\n", av[counter]);
+		printf("%s\n", string_return[counter]);
 	}
 
-	return (0);
+	return (*string_return);
 }
